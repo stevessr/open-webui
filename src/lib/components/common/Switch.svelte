2d4b2c9b9b91b15a97e841dc5f4f1cf19f36e9ac
stevessr <steve-ssr@outlook.com>
2025-09-20 10:25:19 +0800

    fix

--- src/lib/components/common/Switch.svelte
+++ src/lib/components/common/Switch.svelte
@@ -3,23 +3,19 @@
 
 	import { createEventDispatcher, tick, getContext } from 'svelte';
 	import { settings } from '$lib/stores';
-
 	import Tooltip from './Tooltip.svelte';
 	export let state = true;
 	export let id = '';
+	export let ariaLabelledbyId = '';
+	export let tooltip = false;
 
+	const i18n = getContext('i18n');
 	const dispatch = createEventDispatcher();
 </script>
 
-<Switch.Root
-	bind:checked={state}
-	{id}
-	class="flex h-5 min-h-5 w-9 shrink-0 cursor-pointer items-center rounded-full px-[3px] mx-[1px] transition  {($settings?.highContrastMode ??
-	false)
-		? 'focus:outline focus:outline-2 focus:outline-gray-800 focus:dark:outline-gray-200'
-		: 'outline outline-1 outline-gray-100 dark:outline-gray-800'} {state
-		? ' bg-emerald-600'
-		: 'bg-gray-200 dark:bg-transparent'}"
+<Tooltip
+	content={tooltip ? (state ? $i18n.t('Enabled') : $i18n.t('Disabled')) : ''}
+	placement="top"
 >
 	<Switch.Root
 		bind:checked={state}
