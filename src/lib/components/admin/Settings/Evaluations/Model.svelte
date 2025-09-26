afbb589d54113cf0b9a7b8a12672f23ea1c45134
Timothy Jaeryang Baek <tim@openwebui.com>
2025-09-26 16:43:12 -0500

    refac

--- src/lib/components/admin/Settings/Evaluations/Model.svelte
+++ src/lib/components/admin/Settings/Evaluations/Model.svelte
@@ -47,7 +47,7 @@
 
 				<div class="w-full flex flex-col">
 					<div class="flex items-center gap-1">
-						<div class="shrink-0 line-clamp-1">
+						<div class=" line-clamp-1">
 							{model.name}
 						</div>
 					</div>
