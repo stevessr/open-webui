d6f0ed3a1fa13e1dce95d7718dcb64763f4cacb0
Timothy Jaeryang Baek <tim@openwebui.com>
2025-10-07 22:56:52 -0500

    refac/fix: verify connection

--- src/lib/components/AddConnectionModal.svelte
+++ src/lib/components/AddConnectionModal.svelte
@@ -88,10 +88,13 @@
 		// remove trailing slash from url
 		url = url.replace(/\/$/, '');
 
+		let _headers = null;
+
 		if (headers) {
 			try {
-				const _headers = JSON.parse(headers);
+				_headers = JSON.parse(headers);
 				if (typeof _headers !== 'object' || Array.isArray(_headers)) {
+					_headers = null;
 					throw new Error('Headers must be a valid JSON object');
 				}
 				headers = JSON.stringify(_headers, null, 2);
@@ -110,7 +113,7 @@
 					auth_type,
 					azure: azure,
 					api_version: apiVersion,
-					headers: JSON.parse(headers)
+					...(_headers ? { headers: _headers } : {})
 				}
 			},
 			direct
