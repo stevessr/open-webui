6712656ea6036a617dc5407693c85b70d8aaefa7
silentoplayz <jacwoo21@outlook.com>
2025-10-10 21:12:27 -0400

    fix(sidebar): prevent click on Chats from going to new chat

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -983,7 +983,6 @@
 					chevron={false}
 					on:change={async (e) => {
 						selectedFolder.set(null);
-						await goto('/');
 					}}
 					on:import={(e) => {
 						importChatHandler(e.detail);
