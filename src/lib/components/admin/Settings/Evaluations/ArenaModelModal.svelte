0a47f0fd8ff5fd42da056d314dcbd1cb6b9e7a40
stevessr <steve-ssr@outlook.com>
2025-11-17 11:10:27 +0800

    css

--- src/lib/components/admin/Settings/Evaluations/ArenaModelModal.svelte
+++ src/lib/components/admin/Settings/Evaluations/ArenaModelModal.svelte
@@ -235,7 +235,7 @@
 								/>
 
 								<div
-									class="absolute flex justify-center rounded-full bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gray-700 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-50"
+									class="trans absolute flex justify-center rounded-full bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-gray-700 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-50"
 								>
 									<div class="my-auto text-white">
 										<PencilSolid className="size-4" />
