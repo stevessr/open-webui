cc8ad004bf25ce679e499f5f1000fc7812f3767f
stevessr <steve-ssr@outlook.com>
2025-11-18 19:19:40 +0800

    优化 DropdownMenu 组件的样式，更新类名为 transv2

--- src/lib/components/chat/MessageInput/InputMenu.svelte
+++ src/lib/components/chat/MessageInput/InputMenu.svelte
@@ -130,7 +130,7 @@
 
 	<div slot="content">
 		<DropdownMenu.Content
-			class="trans w-full max-w-70 rounded-2xl px-1 py-1  border border-gray-100  dark:border-gray-800 z-50 bg-white dark:bg-gray-850 dark:text-white shadow-lg max-h-72 overflow-y-auto overflow-x-hidden scrollbar-thin transition"
+			class="transv2 w-full max-w-70 rounded-2xl px-1 py-1  border border-gray-100  dark:border-gray-800 z-50 bg-white dark:bg-gray-850 dark:text-white shadow-lg max-h-72 overflow-y-auto overflow-x-hidden scrollbar-thin transition"
 			sideOffset={4}
 			alignOffset={-6}
 			side="bottom"
