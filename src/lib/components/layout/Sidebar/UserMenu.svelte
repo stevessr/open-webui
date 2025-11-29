1c0a75843577c4d011a1067ff4754dc1d0f65c67
stevessr <steve-ssr@outlook.com>
2025-11-29 08:30:16 +0800

    fix

--- src/lib/components/layout/Sidebar/UserMenu.svelte
+++ src/lib/components/layout/Sidebar/UserMenu.svelte
@@ -29,8 +29,6 @@
 	export let help = false;
 	export let className = 'max-w-[240px]';
 
-	export let showActiveUsers = true;
-
 	const dispatch = createEventDispatcher();
 
 	let usage = null;
@@ -66,46 +64,12 @@
 
 	<slot name="content">
 		<DropdownMenu.Content
-			id="user-menu"
 			class="w-full {className}  rounded-2xl px-1 py-1  border border-gray-100  dark:border-gray-800 z-50 bg-white dark:bg-gray-850 dark:text-white shadow-lg text-sm"
 			sideOffset={4}
 			side="top"
 			align="end"
 			transition={(e) => fade(e, { duration: 100 })}
 		>
-			{#if !help && usage && usage?.user_ids?.length > 0}
-				<Tooltip
-					content={usage?.model_ids && usage?.model_ids.length > 0
-						? `${$i18n.t('Running')}: ${usage.model_ids.join(', ')} ✨`
-						: ''}
-				>
-					<div
-						class="flex rounded-xl py-1 px-3 text-xs gap-2.5 items-center"
-						on:mouseenter={() => {
-							getUsageInfo();
-						}}
-					>
-						<div class=" flex items-center">
-							<span class="relative flex size-2">
-								<span
-									class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"
-								></span>
-								<span class="relative inline-flex rounded-full size-2 bg-green-500"></span>
-							</span>
-						</div>
-						<div class=" ">
-							<span class="">
-								{$i18n.t('Active Users')}:
-							</span>
-							<span class=" font-semibold">
-								{usage?.user_ids?.length}
-							</span>
-						</div>
-					</div>
-				</Tooltip>
-				<hr class=" border-gray-50 dark:border-gray-800 my-1 p-0" />
-			{/if}
-
 			<DropdownMenu.Item
 				class="flex rounded-xl py-1.5 px-3 w-full hover:bg-gray-50 dark:hover:bg-gray-800 transition cursor-pointer"
 				on:click={async () => {
@@ -255,8 +219,8 @@
 				<div class=" self-center truncate">{$i18n.t('Sign Out')}</div>
 			</DropdownMenu.Item>
 
-			{#if showActiveUsers && usage}
-				{#if usage?.user_count}
+			{#if usage}
+				{#if usage?.user_ids?.length > 0}
 					<hr class=" border-gray-50 dark:border-gray-800 my-1 p-0" />
 
 					<Tooltip
@@ -284,20 +248,12 @@
 									{$i18n.t('Active Users')}:
 								</span>
 								<span class=" font-semibold">
-									{usage?.user_count}
+									{usage?.user_ids?.length}
 								</span>
 							</div>
 						</div>
-						<div class=" ">
-							<span class="">
-								{$i18n.t('Active Users')}:
-							</span>
-							<span class=" font-semibold">
-								{usage?.user_ids?.length}
-							</span>
-						</div>
-					</div>
-				</Tooltip>
+					</Tooltip>
+				{/if}
 			{/if}
 
 			<!-- <DropdownMenu.Item class="flex items-center py-1.5 px-3 text-sm ">
