1c5efca67dc777d9e873bdd96c0019992a023174
stevessr <steve-ssr@outlook.com>
2025-12-04 16:53:07 +0800

    fix

--- src/lib/components/chat/Settings/About.svelte
+++ src/lib/components/chat/Settings/About.svelte
@@ -83,51 +83,7 @@
 						<div>{$i18n.t("See what's new")}</div>
 					</button>
 				</div>
-
-				{#if $config?.features?.enable_version_update_check}
-					<button
-						class=" text-xs px-3 py-1.5 bg-gray-100 hover:bg-gray-200 dark:bg-gray-850 dark:hover:bg-gray-800 transition rounded-lg font-medium"
-						on:click={() => {
-							checkForVersionUpdates();
-						}}
-					>
-						{$i18n.t('Check for updates')}
-					</button>
-				{/if}
 			</div>
-		</div>
-
-		{#if ollamaVersion}
-			<hr class=" border-gray-100/30 dark:border-gray-850/30" />
-
-			<div>
-				<div class=" mb-2.5 text-sm font-medium">{$i18n.t('Ollama Version')}</div>
-				<div class="flex w-full">
-					<div class="flex-1 text-xs text-gray-700 dark:text-gray-200">
-						{ollamaVersion ?? 'N/A'}
-					</div>
-				</div>
-			</div>
-		{/if}
-
-		<hr class=" border-gray-100/30 dark:border-gray-850/30" />
-
-		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
-			Emoji graphics provided by
-			<a href="https://github.com/jdecked/twemoji" target="_blank">Twemoji</a>, licensed under
-			<a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">CC-BY 4.0</a>.
-		</div>
-
-</pre>
-		</div>
-
-		<div class="mt-2 text-xs text-gray-400 dark:text-gray-500">
-			{$i18n.t('Created by')}
-			<a
-				class=" text-gray-500 dark:text-gray-300 font-medium"
-				href="https://github.com/tjbck"
-				target="_blank">Timothy J. Baek</a
-			>
 		</div>
 	</div>
 </div>
